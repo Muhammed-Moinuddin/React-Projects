4d7d1d3eccd471ba08f75af057be6e9aad10d6ba
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2023-01-24 08:17:13 +0500

    Update App.js
    Used useEffect Hooks and added a functionality.

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,15 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import ContactHeader from "./ContactHeader";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-
+  const LOCAL_STORAGE_KEY = "contactsKey";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact])
   };
+
+  
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
 // const contacts = [
 //     {
 //       id: "1",
